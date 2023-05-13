92f03a445605e0bea797e542cb6989d12743e629
jyothi2718 <jyothirmaipasupuleti22@gmail.com>
2023-05-13 20:23:45 +0530

    task completed

--- script.js
+++ script.js
@@ -8,20 +8,43 @@ let arr = [
 
 function PrintStudentswithMap() {
   //Write your code here , just console.log
+  arr.map(function(student){
+    if( student.marks > 50 ){
+      console.log(student);
+    }
+  });
 }
+  
 
 function PrintStudentsbyForEach() {
   //Write your code here , just console.log
+  arr.forEach((student) => {
+    if (student.marks > 50) {
+      console.log(student);
+    }
+  });
 }
 
 function addData() {
   //Write your code here, just console.log
+  let newStudent = { id: 4, name: "susan", age: "20", marks: 45 };
+  arr.push(newStudent);
+  console.log(arr);
 }
 
 function removeFailedStudent() {
   //Write your code here, just console.log
+  arr = arr.filter((student) => student.marks >= 50);
+  console.log(arr);
 }
 
 function concatenateArray() {
   //Write your code here, just console.log
+  let newArray = [
+    { id: 4, name: "jane", age: "19", marks: 70 },
+    { id: 5, name: "peter", age: "18", marks: 60 },
+    { id: 6, name: "david", age: "20", marks: 55 },
+  ];
+  arr = arr.concat(newArray);
+  console.log(arr);
 }
